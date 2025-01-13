a8495fb06d63adf073fd525f7b441ac56693e272
Jamshed <juraev@bankplanet9.com>
2025-01-13 18:20:00 +0500

    impl simple console version note manager

--- src/main/java/cmd/Note.java
+++ src/main/java/cmd/Note.java
@@ -1,4 +1,41 @@
 package cmd;
 
 public class Note {
+    int id;
+    String title;
+    String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Note(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String toString() {
+        return "ID: " + id + "\nTitle: " + title + "\nContent: " + content;
+    }
 }
